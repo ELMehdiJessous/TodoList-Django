3e49a6031e0bf9fd5729fb28a2eab44d2580a062
SalmaEssalhi <salmaessalhi2024@gmail.com>
2025-12-07 10:44:53 +0000

    create forms.py +update tasks/models.py +tasks/urls.py +tasks/views.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home ,name="home"),
+    path('', views.index, name='todo'),
+    path('delete/<int:item_id>/', views.remove, name='delete'), 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Todo
+from .forms import TodoForm
 
-# Create your views here.
+def index(request):
 
+    item_list = Todo.objects.order_by("-date")
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo')
+    form = TodoForm()
 
-def home(request):
-    return render(request, "todo/home.html")
+    page = {
+        "forms": form,
+        "list": item_list,
+        "title": "TODO LIST",
+    }
+    return render(request, 'todo/home.html', page)
+
+def remove(request, item_id):
+    item = Todo.objects.get(id=item_id)
+    item.delete()
+    messages.info(request, "item removed !!!")
+    return redirect('todo')
